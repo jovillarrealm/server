34dd4fe6a6f4bff527ad5e6f68820fa0ad21d468
Jorge A VM <javillarrm@eafit.edu.co>
2023-04-06 19:17:16 -0500

    Formato de uso, fix de "address already in use"

--- server.c
+++ server.c
@@ -17,10 +17,17 @@
 #define BAD_REQUEST 400
 #define NOT_FOUND 404
 
-
 // Define la estructura de la petición HTTP:
-typedef struct {
-    enum method {GET, POST, PUT, DELETE, UNSUPPORTED} method;
+typedef struct
+{
+    enum method
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE,
+        UNSUPPORTED
+    } method;
     char *host;
     char *path;
     char version[16];
@@ -28,7 +35,8 @@ typedef struct {
 } http_request;
 
 // Aux para saber el tiempo exactamente
-char *get_current_time() {
+char *get_current_time()
+{
     time_t t = time(NULL);
     struct tm tm = *gmtime(&t);
     char *time_str = malloc(30 * sizeof(char));
@@ -37,13 +45,15 @@ char *get_current_time() {
 }
 
 // Muestra un mensaje de error y sale del programa
-void error(const char *msg) {
+void error(const char *msg)
+{
     perror(msg);
     exit(1);
 }
 
 // Logger
-void logger(const char *message) {
+void logger(const char *message)
+{
     time_t rawtime;
     struct tm *timeinfo;
     char time_str[80];
@@ -53,7 +63,8 @@ void logger(const char *message) {
     strftime(time_str, sizeof(time_str), "%d-%m-%Y %H:%M:%S", timeinfo);
 
     FILE *log_file = fopen("log.txt", "a");
-    if (log_file == NULL) {
+    if (log_file == NULL)
+    {
         error("Error al abrir el archivo de registro");
     }
 
@@ -63,46 +74,64 @@ void logger(const char *message) {
 }
 
 // Analiza la línea de solicitud HTTP para determinar el método, la ruta y el host
-void parse_request_line(char *buffer, http_request *request) {
+void parse_request_line(char *buffer, http_request *request)
+{
     char *method_end = strchr(buffer, ' ');
-    if (method_end == NULL) {
+    if (method_end == NULL)
+    {
         request->method = UNSUPPORTED;
         return;
     }
     size_t method_len = method_end - buffer;
-    if (strncmp(buffer, "GET", method_len) == 0) {
+    if (strncmp(buffer, "GET", method_len) == 0)
+    {
         request->method = GET;
-    } else if (strncmp(buffer, "POST", method_len) == 0) {
+    }
+    else if (strncmp(buffer, "POST", method_len) == 0)
+    {
         request->method = POST;
-    } else if (strncmp(buffer, "PUT", method_len) == 0) {
+    }
+    else if (strncmp(buffer, "PUT", method_len) == 0)
+    {
         request->method = PUT;
-    } else if (strncmp(buffer, "DELETE", method_len) == 0) {
+    }
+    else if (strncmp(buffer, "DELETE", method_len) == 0)
+    {
         request->method = DELETE;
-    } else {
+    }
+    else
+    {
         request->method = UNSUPPORTED;
     }
     printf("--> Método HTTP: %d\n", request->method);
 
     char *uri_start = method_end + 1;
     char *uri_end = strchr(uri_start, ' ');
-    if (uri_end == NULL) {
+    if (uri_end == NULL)
+    {
         request->method = UNSUPPORTED;
         return;
     }
     size_t uri_len = uri_end - uri_start;
     char *http_version_start = uri_end + 1;
-    if (http_version_start[0] != 'H' || http_version_start[1] != 'T' || http_version_start[2] != 'T' || http_version_start[3] != 'P' || http_version_start[4] != '/') {
+    if (http_version_start[0] != 'H' || http_version_start[1] != 'T' || http_version_start[2] != 'T' || http_version_start[3] != 'P' || http_version_start[4] != '/')
+    {
         request->method = UNSUPPORTED;
         printf("-->http_version_start no comenza con HTTP/. ");
         return;
     }
-    if (http_version_start[5] == '0') {
+    if (http_version_start[5] == '0')
+    {
         strcpy(request->version, "HTTP/1.0");
         printf("-->HTTP version 1.0 detectado. ");
-    } else if (http_version_start[5] == '1') {
+    }
+    else if (http_version_start[5] == '1')
+    {
         strcpy(request->version, "HTTP/1.1 ");
         printf("-->HTTP version 1.1 detectado. ");
-    } else {
+    }
+    else
+    {
         request->method = UNSUPPORTED;
         printf("--> No se puede detectar la version de HTTP o no esta soportado. ");
         return;
@@ -110,19 +139,25 @@ void parse_request_line(char *buffer, http_request *request) {
     request->version[7] = '\0';
     char *uri = strndup(uri_start, uri_len);
     char *host_start = strstr(uri, "://");
-    if (host_start) {
+    if (host_start)
+    {
         host_start += 3;
         char *path_start = strchr(host_start, '/');
-        if (path_start) {
+        if (path_start)
+        {
             *path_start = '\0';
             path_start++;
             request->host = strdup(host_start);
             request->path = strdup(path_start);
-        } else {
+        }
+        else
+        {
             request->host = strdup(host_start);
             request->path = strdup("");
         }
-    } else {
+    }
+    else
+    {
         request->host = strdup("");
         request->path = strdup(uri);
     }
@@ -131,13 +166,13 @@ void parse_request_line(char *buffer, http_request *request) {
     free(uri);
 }
 
-
-
 // Función para manejar una conexión de cliente
-void handle_connection(int client_fd) {
+void handle_connection(int client_fd)
+{
     char request_buffer[MAX_REQUEST_SIZE];
     ssize_t bytes_received = recv(client_fd, request_buffer, MAX_REQUEST_SIZE - 1, 0);
-    if (bytes_received < 0) {
+    if (bytes_received < 0)
+    {
         error("Error al recibir la solicitud HTTP");
     }
     request_buffer[bytes_received] = '\0';
@@ -155,47 +190,50 @@ void handle_connection(int client_fd) {
     int response_code = 0;
     char *status_text = NULL;
 
-    switch (request.method) {
-        case GET:
-            response_code = 200;
-            status_text = "OK";
-            response_body = "<html><body><h1>¡Hola, mundo! Si, Funciono</h1></body></html>";
-            response_length = strlen(response_body);
-            break;
-        case POST:
-            response_code = 200;
-            status_text = "OK";
-            response_body = "<html><body><h1>¡Gracias por enviar datos, jeje!</h1></body></html>";
-            response_length = strlen(response_body);
-            break;
-        default:
-            response_code = 400;
-            status_text = "Bad Request";
-            response_body = "<html><body><h1>Solicitud HTTP no válida, ayudame cristooo</h1></body></html>";
-            response_length = strlen(response_body);
-            break;
+    switch (request.method)
+    {
+    case GET:
+        response_code = 200;
+        status_text = "OK";
+        response_body = "<html><body><h1>¡Hola, mundo! Si, Funciono</h1></body></html>";
+        response_length = strlen(response_body);
+        break;
+    case POST:
+        response_code = 200;
+        status_text = "OK";
+        response_body = "<html><body><h1>¡Gracias por enviar datos, jeje!</h1></body></html>";
+        response_length = strlen(response_body);
+        break;
+    default:
+        response_code = 400;
+        status_text = "Bad Request";
+        response_body = "<html><body><h1>Solicitud HTTP no válida, ayudame cristooo</h1></body></html>";
+        response_length = strlen(response_body);
+        break;
     }
 
     // Generar la respuesta HTTP
     char response_buffer[MAX_RESPONSE_SIZE];
     snprintf(response_buffer, MAX_RESPONSE_SIZE,
-         "%s %d %s\r\n"
-         "Content-Type: text/html\r\n"
-         "Content-Length: %zu\r\n"
-         "Host: localhost\r\n"
-         "Date: %s\r\n"
-         "Connection: close\r\n"
-         "\r\n"
-         "%s",
-         (strcmp(request.version, "HTTP/1.1") == 0) ? "HTTP/1.1" : "HTTP/1.0", response_code, status_text, response_length, get_current_time(), response_body);
-         int response_length_written = snprintf(response_buffer, MAX_RESPONSE_SIZE, "%s %d %s\r\nContent-Length: %lu\r\nContent-Type: text/html\r\n\r\n%s", request.version, response_code, status_text, response_length, response_body);
-         if (response_length_written >= MAX_RESPONSE_SIZE) {
-            error("Respuesta HTTP demasiado larga para el búfer");
-        }
+             "%s %d %s\r\n"
+             "Content-Type: text/html\r\n"
+             "Content-Length: %zu\r\n"
+             "Host: localhost\r\n"
+             "Date: %s\r\n"
+             "Connection: close\r\n"
+             "\r\n"
+             "%s",
+             (strcmp(request.version, "HTTP/1.1") == 0) ? "HTTP/1.1" : "HTTP/1.0", response_code, status_text, response_length, get_current_time(), response_body);
+    int response_length_written = snprintf(response_buffer, MAX_RESPONSE_SIZE, "%s %d %s\r\nContent-Length: %lu\r\nContent-Type: text/html\r\n\r\n%s", request.version, response_code, status_text, response_length, response_body);
+    if (response_length_written >= MAX_RESPONSE_SIZE)
+    {
+        error("Respuesta HTTP demasiado larga para el búfer");
+    }
 
     // Enviar la respuesta HTTP al cliente
     ssize_t bytes_sent = send(client_fd, response_buffer, strlen(response_buffer), 0);
-    if (bytes_sent < 0) {
+    if (bytes_sent < 0)
+    {
         error("Error al enviar la respuesta HTTP");
     }
     logger(response_buffer);
@@ -203,13 +241,16 @@ void handle_connection(int client_fd) {
 }
 
 // Función para el hilo que acepta conexiones de clientes
-void *accept_connections(void *server_fd_ptr) {
+void *accept_connections(void *server_fd_ptr)
+{
     int server_fd = *(int *)server_fd_ptr;
-    while (1) {
+    while (1)
+    {
         struct sockaddr_in client_addr;
         socklen_t client_addr_len = sizeof(client_addr);
         int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-        if (client_fd < 0) {
+        if (client_fd < 0)
+        {
             error("Error al aceptar la conexión del cliente");
         }
         handle_connection(client_fd);
@@ -218,22 +259,51 @@ void *accept_connections(void *server_fd_ptr) {
 }
 
 // Funcion para manejar conexiones de varios clientes
-void *connection_handler(void *arg) {
+void *connection_handler(void *arg)
+{
     int client_fd = *(int *)arg;
     handle_connection(client_fd);
     close(client_fd);
     return NULL;
 }
 
-int main(int argc, char *argv[]) {
-    
+// De: https://stackoverflow.com/questions/20019786/safe-and-portable-way-to-convert-a-char-to-uint16-t
+int str_to_uint16(char *str, uint16_t *res)
+{
+    char *end;
+    int errno = 0;
+    long val = strtol(str, &end, 10);
+    if (errno || end == str || *end != '\0' || val < 0 || val >= 0x10000)
+    {
+        return -1;
+    }
+    *res = (uint16_t)val;
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    // Mock de llamado de programa
+    u_int16_t port = 8080;
+    FILE *log_file = fopen("log.txt", "a+");
+    char *doc_root_folder = " .";
+    // Si se llama el programa correctamente, usa esos datos
+    if (argc == 4)
+    {
+        if (str_to_uint16(argv[1], &port) == -1)
+            error("uso: ./server <PORT> <LOG_FILE> <DOC_ROOT_FOLDER>\n");
+        log_file = fopen(argv[2], "a+"); // a+ (create + append) option will allow appending which is useful in a log file
+        doc_root_folder = argv[3];
+    }
+
     int server_fd, client_fd;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
     char buffer[BUFFER_SIZE] = {0};
 
     // Crea el socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
         perror("Error al crear el socket \n");
         exit(EXIT_FAILURE);
     }
@@ -241,32 +311,41 @@ int main(int argc, char *argv[]) {
     // Configura la dirección y el puerto para el socket
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    address.sin_port = htons(port);
+
+    // Lose the pesky "address already in use" error message
+    int _yes = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &_yes, sizeof(_yes));
 
     // Vincula el socket al puerto y la dirección especificados
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
         perror("Error al vincular el socket \n");
         exit(EXIT_FAILURE);
     }
 
     // Escucha las conexiones entrantes
-    if (listen(server_fd, MAX_CONNECTIONS) < 0) {
+    if (listen(server_fd, MAX_CONNECTIONS) < 0)
+    {
         perror("Error al escuchar en el socket \n");
         exit(EXIT_FAILURE);
     }
 
-    printf("Servidor iniciado en el puerto %d...\n", PORT);
+    printf("Servidor iniciado en el puerto %d...\n", port);
 
-    while (1) {
+    while (1)
+    {
         // Acepta una nueva conexión entrante
-        if ((client_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        if ((client_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        {
             perror("Error al aceptar la conexión entrante \n");
             continue;
         }
 
         // Crea un nuevo hilo para manejar la conexión entrante
         pthread_t thread_id;
-        if (pthread_create(&thread_id, NULL, connection_handler, (void *)&client_fd) < 0) {
+        if (pthread_create(&thread_id, NULL, connection_handler, (void *)&client_fd) < 0)
+        {
             perror("Error al crear el hilo \n");
             continue;
         }
