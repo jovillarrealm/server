8ffff370258f14e315b24d89a68430bdc14d755b
Jorge A VM <javillarrm@eafit.edu.co>
2023-04-15 07:33:45 -0500

    bugfix

--- tests/tests.py
+++ tests/tests.py
@@ -2,14 +2,22 @@ import requests
 import concurrent.futures
 
 def send_request(url):
-    count = 0
+    response = requests.get(url)
+    print(response.status_code)
+    print("Done")
+
+def send_stress(url):
+    count =0
     for letter in url:
         response = requests.get(url)
         print(response.status_code)
-        count += 1
+        if response.status_code == 200:
+            count +=1
     print(f"Done: {count}")
-
-urls = ['http://localhost:8080/example.html'] * 10
+port = 8080
+urls = ['http://localhost:{port}'] * 10
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
     executor.map(send_request, urls)
+    print("And now a stress test")
+    executor.map(send_stress, urls)
