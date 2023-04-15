ebb1267a583b9b54225d538784bdb86125ccd22f
Jorge A VM <javillarrm@eafit.edu.co>
2023-04-15 07:29:23 -0500

    Opción de tirar MUCHOS requests

--- tests/tests.py
+++ tests/tests.py
@@ -5,7 +5,16 @@ def send_request(url):
     response = requests.get(url)
     print(response.status_code)
 
+def send_stress(url):
+    count =0
+    for letter in url:
+        response = requests.get(url)
+        print(response.status_code)
+    print(f"Done: {count}")
+    
 urls = ['http://localhost:8080'] * 10
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
     executor.map(send_request, urls)
+    print("And now a stress test")
+    executor.map(send_request)
