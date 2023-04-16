cc11ada7cf2cf0ba9c374d3478d6db247f6cd830
srodriguev <srodriguev@eafit.edu.co>
2023-04-16 18:37:16 -0500

    its own file

---
+++ saveArchivos.h
@@ -0,0 +1,20 @@
+#ifndef SAVEARCHIVOS_H
+#define SAVEARCHIVOS_H
+
+// #includes
+
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "http_request.h"
+
+// Functions
+
+void saveFile(http_request *request); 
+
+// End of header file
+#endif
