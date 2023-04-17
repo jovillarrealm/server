9c255594cfbd478d52ef0a4b277ee74c4201dc29
srodriguev <srodriguev@eafit.edu.co>
2023-04-17 13:59:12 -0500

    http struct update

--- http_request.h
+++ http_request.h
@@ -16,6 +16,24 @@ typedef struct http_request
     char *body;
     char version[16];
     int status_code;
+    int content_len;
+    char *content_type;
 } http_request;
 
+void prequest(http_request req)
+{
+
+    printf("--Method: %d\n", req.method);
+    if (req.host != NULL);
+        printf("--Host: %s\n", req.host);
+    if (req.path!=NULL)
+        printf("--Path: %s\n", req.path);
+    if (req.body!=NULL)
+        printf("--Body: %s\n", req.body);
+    //if (req.status_code)    
+    if (req.content_len!=0)
+        printf("--Content-Length: %d\n", req.content_len);
+    if (req.content_type)
+        printf("--Content-Type: %s\n", req.content_type);
+}
 #endif
